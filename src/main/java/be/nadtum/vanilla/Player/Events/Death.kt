68ec820c4a5087ca425da770cf22913149fc1bab
NadTum <akianadtum@gmail.com>
2023-06-17 10:30:40 +0200

    début de la gestion de la mort

--- src/main/java/be/nadtum/vanilla/Player/Events/Death.kt
+++ src/main/java/be/nadtum/vanilla/Player/Events/Death.kt
@@ -1,7 +1,17 @@
 package be.nadtum.vanilla.Player.Events
 
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
 
 class Death : Listener {
+
+    @EventHandler
+    fun onDeath(event: PlayerDeathEvent) {
+
+        val player = event.entity
+        val message = event.deathMessage
+
+    }
 
 }
